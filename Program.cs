c4c1a04be810934ac28a3223196591d91e245b96
CaceresNatalia <caceresnataliaandrea@gmail.com>
2023-03-24 16:57:39 -0300

    Práctica POO con C#

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo4
+{
+    class Program
+    {
+
+    //        Crear algunos objetos en el main de Program y probar escribirle datos y mostrar en pantalla
+    //        el estado del Telefono.
+    //Agregar método Llamar() sin parámetros que devuelva un string con la leyenda "Realizando llamada...".
+    //Sobrecargar el método Llamar(string contacto) para que reciba un contacto y devuelva un string
+    //con la leyenda "Llamando a " + contacto
+    //Probar métodos en el main mostrando en pantalla el comportamiento de los objetos.
+
+        static void Main(string[] args)
+        {
+
+            Telefono t1 = new Telefono();
+            t1.NumeroTelefonico = "15304584569";
+
+            Telefono t2 = new Telefono("Redmi Note", "Xiaomi");
+            t2.CodigoOperador= 1;
+            t1.CodigoOperador = 4;
+            Console.WriteLine("la marca es " + t2.Marca);
+            Console.WriteLine(t1.NumeroTelefonico);
+            Console.WriteLine("El código del primero es " + t1.CodigoOperador);
+            Console.WriteLine("El Código del 2do es " + t2.CodigoOperador);
+            
+
+            Console.WriteLine(t1.Llamar());
+
+            Console.WriteLine(t2.Llamar("Pablo"));
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Telefono.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo4
+{
+//    Crear un proyecto de consola.Net Framework.
+//Crear la clase Telefono (recordemos que una clase va en un archivo nuevo; click derecho en el proyecto, agregar, class).
+//Agregale los siguientes atributos:
+//Modelo string.
+//Marca string.
+//NumeroTelefonico string.
+//CodigoOperador int (1, 2 o 3).
+//Agregale las propiedades correspondientes.Podés hacer el formato largo o el corto.
+//Modelo: solo lectura. Es decir, solo get.
+//Marca: solo lectura.
+//NumeroTelefonico: lectura y escritura.
+//CodigoOperador: lectura y escritura.Validar escritura que solo admita 1, 2 o 3, caso contrario escribir un cero.
+//Agregar Constructor que reciba Modelo y Marca.
+
+    class Telefono
+    {
+        public Telefono(string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+        public Telefono() { }
+
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperador;
+
+       
+
+        public string Modelo {
+            get { return modelo; } }
+        
+
+        public string Marca {
+
+            get { return marca; } }
+
+        public string NumeroTelefonico { get; set; }
+
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+
+            set
+            {
+                if (value == 1 || value == 2 || value == 3)
+                    codigoOperador = value;
+                else
+                    codigoOperador = 0;
+            }
+        }
+
+        public string Llamar()
+        {
+            return "Realizando llamada...";
+        }
+
+        
+        public string Llamar(string contacto)
+        {
+            return "Llamando a " +  contacto;
+        }
+
+       
+
+        
+
+
+
+        }
+
+
+
+        
+    }
+
+
